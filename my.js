7bbef1fb30142e4451cf427178147b639b567abf
kgpeth <37132847+kgpeth@users.noreply.github.com>
2020-07-18 00:32:35 -0500

    Update my.js

--- my.js
+++ my.js
@@ -34,7 +34,7 @@ function addGS() {
         },
         redirect: 'follow', // manual, *follow, error
        // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify({name:"Olivia"}) // body data type must match "Content-Type" header
+        body: JSON.stringify({First:"ana",Last:"Cell 50",Telephone:89800097009}) // body data type must match "Content-Type" header
       });
 }
 
