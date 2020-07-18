8e60a6cae243a6cd307ae277839a263b5c110da5
kgpeth <37132847+kgpeth@users.noreply.github.com>
2020-07-17 23:03:10 -0500

    Update my.js

--- my.js
+++ my.js
@@ -1,6 +1,6 @@
 function testGS() {
 
-    const url = "https://script.google.com/macros/s/AKfycbx3P3daXkxLDJBw6i-S5p3gCuJWyHghIoZmVy0ocCqWfXNdu5FA/exec"
+    const url = "https://script.google.com/macros/s/AKfycbx3P3daXkxLDJBw6i-S5p3gCuJWyHghIoZmVy0ocCqWfXNdu5FA/exec";
     
     //"https://script.googleusercontent.com/macros/echo?user_content_key=Xv3qa44odCYZpW3yNMX7BaYUJOgYyIcmeIx328aK4Vg1_OdHNXpovEXrJ10PNDCF_SJESzW9nSHnMwwBPlNJbuuJIom04DWHm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnNcbIvMeL7C3wexBJk4CjY950cJY6FMLim5QZOUVNXT4l7lD0rMCzwZgioWaTV97WOsaBFSPXFn7&lib=MIctwdw7rX-HdG-SP3F0HtktBEwHaOX08";
       
